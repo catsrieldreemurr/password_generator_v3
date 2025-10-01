f6ac30d8e2737ec7a2e71a84a997346df4137e92
StargazerCatsriel <danbro07@imkatta.no>
2025-10-01 09:07:50 +0200

    Add Multiple Pages
    I LOVE NEXT.JS

---
+++ passwordgenerator/app/generator/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../navbar";
+
+function GeneratePasswords(){
+    return(
+       <div>
+        <Navbar></Navbar>
+        <h1>test</h1>
+       </div>
+    )
+}
+
+export default GeneratePasswords;
--- passwordgenerator/app/navbar.tsx
+++ passwordgenerator/app/navbar.tsx
@@ -6,8 +6,8 @@ function Navbar(){
         <nav className="bg-sky-800 p-3">
             <ul className="flex gap-10 items-center">
                 <li> <Image src={"/catsrieldreemurr.png"} alt="catsrieldreemurr" width="200" height="200"></Image></li>
-                <Link href="https://Youtube.com" className="text-white p-2 rounded-sm hover:bg-sky-700">Information</Link>
-                <Link href="https://Youtube.com" className="text-white p-2 rounded-sm hover:bg-sky-700">Generate</Link>
+                <li> <Link href="/" className="text-white p-2 rounded-sm hover:bg-sky-700">Information</Link></li>
+                <li> <Link href="/generator" className="text-white p-2 rounded-sm hover:bg-sky-700">Generate</Link></li>
             </ul>
         </nav>
     )
