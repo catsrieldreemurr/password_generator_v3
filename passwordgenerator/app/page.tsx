69b1331a4fd0cde206a28cb6aaf366c3146c9275
Vultus Limunaria <danobrovold@gmail.com>
2025-10-03 14:50:14 +0200

    Fix the centering issue
    jesus fucking christ

--- passwordgenerator/app/page.tsx
+++ passwordgenerator/app/page.tsx
@@ -4,10 +4,10 @@ import Footerbar from "./components/footerbar";
 
 export default function Home() {
   return (
-    <div className="bg-cyan-900 h-screen bg-cover">
+    <div className="bg-cyan-900 h-screen bg-cover flex flex-col">
       <Navbar></Navbar>
 
-      <div className="flex flex-col justify-center items-center bg-red-900">
+      <div className="flex flex-1 flex-col justify-center items-center text-center">
         <h1 className="text-4xl font-bold sm:text-5xl">Catsriel's Password Generator V3</h1>
         <h2 className="text-xl sm:text-2xl">The World's Worst Password Generator, 3 Times in a Row</h2>
       </div>
