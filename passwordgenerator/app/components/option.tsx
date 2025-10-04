4144ee8e68895e78c2e48ae097515d5808d91b94
Vultus Limunaria <danobrovold@gmail.com>
2025-10-04 15:18:58 +0200

    Start Working on a Option Bar
    Logic needs to be cleaned up and neatened.
    
    Option needs to be probably cleaned up a lot. lol

---
+++ passwordgenerator/app/components/option.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import { types } from "util";
+import Text from "./text";
+
+type optionChildren = {
+   variant: string,
+   optionText:string,
+   
+}
+
+function Optionbox({variant, optionText}: optionChildren){
+    if(variant === "bool"){
+        function onClickFunc(){
+            setValue(!value)
+            console.log(value);
+        }
+        
+        const [value, setValue] = useState(false)
+        return(
+            <div className="flex p-5 items-center gap-5">
+                {value ? 
+                <button className="bg-green-400 p-6 pt-3 pb-3 rounded-xl border" onClick={onClickFunc}>Enabled</button> 
+                : 
+                <button className="bg-red-800 p-6 pt-3 pb-3 rounded-xl border" onClick={onClickFunc}>Disabled</button>}
+                <Text>{optionText}</Text>
+            </div>
+        )
+    }
+}
+
+export default Optionbox;
--- passwordgenerator/app/generator/page.tsx
+++ passwordgenerator/app/generator/page.tsx
@@ -1,11 +1,30 @@
+import Footerbar from "../components/footerbar";
 import Navbar from "../components/navbar"
+import Optionbox from "../components/option";
+import Subheader from "../components/subheader";
 
 function GeneratePasswords(){
     return(
-    <div>
+    <section className="bg-cyan-900 h-screen bg-cover">
         <Navbar></Navbar>
-        <h1>test</h1>
-    </div>
+        
+        <div className="sm:flex">
+            <div id="settings" className="bg-gray-900 sm:w-1/3">
+                <Subheader>Settings</Subheader>
+                <Optionbox variant="bool" optionText="Enable Readable Passwords"></Optionbox>
+            </div>
+
+            <div id="generator" className="sm:w-1/3">
+                <Subheader>Generate</Subheader>
+            </div>
+
+            <div id="information" className="sm:w-1/3">
+                <Subheader>Statistics</Subheader>
+            </div>
+        </div>
+        
+        <Footerbar/>
+    </section>
     )
 }
 
